a3d2ccfb1fb695532274050f0ae2f427e659e7ea
Micky Ching <mickyching@gmail.com>
2018-03-07 16:31:47 +0800

    add httpserve
    Signed-off-by: Micky Ching <mickyching@gmail.com>

--- mreqs.go
+++ mreqs.go
@@ -8,6 +8,25 @@ import (
 	"time"
 )
 
+// HttpServe run a http server
+// addr format 0.0.0.0:8080
+// performance test tool and setting
+// **socket: too many open files**
+//   ulimit -a | grep open      // shows 1024
+//   ulimit -n 65535            // set a large number
+// **connect: cannot asign requested address
+//   cat /proc/net/sockstat     // shows socket usage status
+// **apr_socket_recv: Connection reset by peer**
+//   server has too many connections possible syn flooding
+//   set server /etc/sysctl.conf: net.ipv4.tcp_syncookies = 0
+// **profile using pprof and graphviz**
+//   go tool pprof ./binary URL/debug/pprof/profile // CPU-profile, MEM-heap
+//   usful cmd: top10/web [func]/list [func]
+func HttpServe(addr string, route func(w http.ResponseWriter, r *http.Request)) error {
+	http.HandleFunc("/", route)
+	return http.ListenAndServe(addr, nil)
+}
+
 func HttpPost(url string, reqs, resp interface{}, timeout int) error {
 	body, err := json.Marshal(reqs)
 	if err != nil {
